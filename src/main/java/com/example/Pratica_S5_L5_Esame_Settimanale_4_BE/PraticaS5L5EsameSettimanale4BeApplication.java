55c38ec2fceba319db6ba270778fd7a9bb80431b
Marcello Miranda <marcellomiranda2004@gmail.com>
2025-06-06 10:02:07 +0200

    Initial commit

---
+++ src/main/java/com/example/Pratica_S5_L5_Esame_Settimanale_4_BE/PraticaS5L5EsameSettimanale4BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Pratica_S5_L5_Esame_Settimanale_4_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticaS5L5EsameSettimanale4BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticaS5L5EsameSettimanale4BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Pratica_S5_L5_Esame_Settimanale_4_BE/PraticaS5L5EsameSettimanale4BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Pratica_S5_L5_Esame_Settimanale_4_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticaS5L5EsameSettimanale4BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
